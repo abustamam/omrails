c2a86fe2bd890657c8bda400ccc35c7c8fcafe8e
abustamam <Rasheed@Blog2Success.net>
2013-04-03 10:11:52 -0700

    Add Endless Scroll

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @pins = @user.pins.page(params[:page]).per_page(20)
+    respond_to do |format|
+    	format.js
+    end
   end
 end
